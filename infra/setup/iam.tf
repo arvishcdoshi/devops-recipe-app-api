40e5faf6de194d54081f702d1d9ceb5bd8cdae95
arvishcdoshi <doshi.arvish16@gmail.com>
2024-08-08 13:08:04 +0530

    [FIX] - Added iam:CreateServiceLinkedRole to fix Error Creating DB Instance:InvalidParameterValue issue

--- infra/setup/iam.tf
+++ infra/setup/iam.tf
@@ -179,7 +179,8 @@ data "aws_iam_policy_document" "rds" {
       "rds:DeleteDBInstance",
       "rds:ListTagsForResource",
       "rds:ModifyDBInstance",
-      "rds:AddTagsToResource"
+      "rds:AddTagsToResource",
+      "iam:CreateServiceLinkedRole"
     ]
     resources = ["*"]
   }
